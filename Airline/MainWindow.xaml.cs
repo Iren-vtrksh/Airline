babfe58d0e82d613c4a1b0c35a3b3dd0b7e87693
Iren <vytrykush.iryna@gmail.com>
2022-05-08 16:55:24 +0300

    Add new Window WDESIGN.xaml.cs

--- Airline/MainWindow.xaml.cs
+++ Airline/MainWindow.xaml.cs
@@ -24,5 +24,33 @@ namespace Airline
         {
             InitializeComponent();
         }
+
+        private void RegistrationBut_Click(object sender, RoutedEventArgs e)
+        {
+            WREGISTRATION1 wREGISTRATION1 = new WREGISTRATION1();
+            wREGISTRATION1.Show();
+            Hide();
+        }
+
+        private void EnterBut_Click(object sender, RoutedEventArgs e)
+        {
+            WENTER1 wENTER1 = new WENTER1();
+            wENTER1.Show();
+            Hide();
+        }
+
+        private void OlderBut_Click(object sender, RoutedEventArgs e)
+        {
+            WDESIGN wDESIGN = new WDESIGN();
+            wDESIGN.Show();
+            Hide();
+        }
+
+        private void BookingBut_Click(object sender, RoutedEventArgs e)
+        {
+            WBOOKING1 wBOOKING1 = new WBOOKING1();
+            wBOOKING1.Show();
+            Hide();
+        }
     }
 }
---
+++ Airline/WENTER1.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Airline
+{
+    /// <summary>
+    /// Логика взаимодействия для WENTER1.xaml
+    /// </summary>
+    public partial class WENTER1 : Window
+    {
+        public WENTER1()
+        {
+            InitializeComponent();
+        }
+
+        private void EnterBut2_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            Hide();
+        }
+    }
+}
